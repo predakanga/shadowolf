202d3772b5c024bd3dacc1837ccadbe8f5f22404
oorza2k5@gmail.com <oorza2k5@gmail.com@d2126afe-2272-11df-8901-e16673284e38>
2010-03-04 08:29:05 +0000

    Switchers seeders from ConcurrentHashMap<String, ConcurrentHashMap> to ConcurrentHashMap<String, Vector<Object[]>> for obvious reasons related to getSeeders().  While Vector is not threadsafe, the objects it contains should always be final, so the only actual write operations will be on the Vector itself, which gets a lock from ConcurrentHashMap.

--- src/com/shadowolf/tracker/PeerList.java
+++ src/com/shadowolf/tracker/PeerList.java
@@ -3,6 +3,7 @@ package com.shadowolf.tracker;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
+import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PeerList {
@@ -14,8 +15,8 @@ public class PeerList {
 	 * The top level key->val is info_hash(String) -> Peer information(ConcurrentHashMap)
 	 * The second level key->val is IP -> Port
 	 */
-	private ConcurrentHashMap<String, ConcurrentHashMap<String, Integer>> seeders = 
-		new ConcurrentHashMap<String, ConcurrentHashMap<String, Integer>>();
+	private ConcurrentHashMap<String, Vector<Object[]>> seeders = 
+		new ConcurrentHashMap<String, Vector<Object[]>>();
 	
 	private ConcurrentHashMap<String, ConcurrentHashMap<String, Integer>> leechers = 
 		new ConcurrentHashMap<String, ConcurrentHashMap<String, Integer>>();
@@ -25,14 +26,13 @@ public class PeerList {
 			
 	}
 	
-	@SuppressWarnings("unchecked")
-	public ConcurrentHashMap<String, Integer>[] getSeeders(final String info_hash, final int numPeers) {
+	public Object[][] getSeeders(final String info_hash, final int numPeers) {
 		final int size = this.seeders.get(info_hash).size();
 		
 		//make sure there are more peers wanted than there are seeders
 		//we need to go ahead and return this or we'll have a forever-true whileloop later.
 		if(numPeers > size) {
-			return (ConcurrentHashMap<String, Integer>[]) this.seeders.get(info_hash).values().toArray();
+			return (Object[][]) this.seeders.get(info_hash).toArray();
 		}
 		
 		//this is a sad little optimization that we have to prevent our while loop from taking
@@ -42,26 +42,19 @@ public class PeerList {
 		//or when the number of peers is more than 2/3 the complete size.
 		if(numPeers + 10 > size || (numPeers*3)/2 > size) {
 			final int rand = (new Random()).nextInt(size - numPeers);
-			return (ConcurrentHashMap<String, Integer>[]) 
-				Arrays.copyOfRange(this.seeders.get(info_hash).values().toArray(), rand, numPeers+rand-1);
+			return (Object[][]) this.seeders.get(info_hash).subList(rand, numPeers-rand-1).toArray();
 		}
 		
 		//This is a temporary hash-set that we use to store the peers we're going to be 
 		//returning.  We use a hash set to ensure that peers don't get added more than once.
-		HashSet<ConcurrentHashMap<String, Integer>> peers = 
-			new HashSet<ConcurrentHashMap<String,Integer>>(numPeers);
-		
-		//because there's no way to retrieve random values from a ConcurrentHashMap, nor
-		//can we retrieve values by numerical key, instead we have to copy the array of values
-		//entirely in order to accomplish that
-		final ConcurrentHashMap<String, Integer>[] temp =
-			(ConcurrentHashMap<String, Integer>[])this.seeders.get(info_hash).values().toArray();
+		HashSet<Object> peers = new HashSet<Object>(numPeers);
 		
 		//finally, we loop, generating random numbers, until we have enough peers
 		while(peers.size() < numPeers) {
-			peers.add(temp[(new Random()).nextInt(temp.length) - 1]);
+			final int rand = (new Random()).nextInt(size);
+			peers.add(this.seeders.get(info_hash).get(rand));
 		}
 		
-		return (ConcurrentHashMap<String, Integer>[]) peers.toArray();
+		return (Object[][]) peers.toArray();
 	}
 }

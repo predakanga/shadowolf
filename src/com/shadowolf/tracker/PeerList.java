7d2d7c2de6b6b4b5f90fd7759a21abf992b6c444
oorza2k5@gmail.com <oorza2k5@gmail.com@d2126afe-2272-11df-8901-e16673284e38>
2010-03-06 01:07:40 +0000

    Should be "done".

--- src/com/shadowolf/tracker/PeerList.java
+++ src/com/shadowolf/tracker/PeerList.java
@@ -1,38 +1,100 @@
 package com.shadowolf.tracker;
 
-import java.util.Arrays;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PeerList {
-	/*
-	 * Peers maps
-	 * 
-	 * Peers is a map of maps.
-	 * 
-	 * The top level key->val is info_hash(String) -> Peer information(ConcurrentHashMap)
-	 * The second level key->val is IP -> Port
-	 */
-	private ConcurrentHashMap<String, Vector<Object[]>> seeders = 
-		new ConcurrentHashMap<String, Vector<Object[]>>();
+	//TODO: vector constructor parameter tuning
+	private final static int SEEDER_START = 64;
+	private final static int SEEDER_GROW = 16;
+	private final static int LEECHER_START = 64;
+	private final static int LEECHER_GROW = 16;
 	
-	private ConcurrentHashMap<String, Vector<Object[]>> leechers = 
-		new ConcurrentHashMap<String, Vector<Object[]>>();
+	//TODO: initial size tuning		
+	private ConcurrentHashMap<String, Vector<Peer>> seeders = 
+		new ConcurrentHashMap<String, Vector<Peer>>(1024, 0.66F, 24);
 	
-	public void addUpdate(final String info_hash, final String IP, 
-				final int port, final String event) {
-			
+	private ConcurrentHashMap<String, Vector<Peer>> leechers = 
+		new ConcurrentHashMap<String, Vector<Peer>>(1024, 0.66F, 24);
+	
+	public PeerList() {}
+	
+	public void remove(final String info_hash, final String IP,
+				final int port, final boolean isSeeder) {
+		final Peer peer = new Peer();
+		peer.setIpAddress(IP);
+		peer.setLastAnnounce(new Date());
+		peer.setPort(port);
+		
+		this.remove(info_hash, peer, isSeeder);
 	}
 	
-	public Object[][] getSeeders(final String info_hash, final int numPeers) {
-		final int size = this.seeders.get(info_hash).size();
+	public void remove(final String info_hash, final Peer peer, final boolean isSeeder) {
+		if(isSeeder) {
+			if(this.seeders.get(info_hash) == null) {
+				return;
+			}
+			
+			this.seeders.get(info_hash).remove(peer);
+		} else {
+			if(this.leechers.get(info_hash) == null) {
+				return;
+			}
+			
+			this.leechers.get(info_hash).remove(peer);
+		}
+	}
+	
+	public void add(final String info_hash, final String IP, 
+				final int port, final boolean isSeeder) {
+		final Peer peer = new Peer();
+		peer.setIpAddress(IP);
+		peer.setLastAnnounce(new Date());
+		peer.setPort(port);
+		
+		this.add(info_hash, peer, isSeeder);
+	}
+	
+	public void add(final String info_hash, final Peer peer, final boolean isSeeder) {
+		if(isSeeder) {
+			if(this.seeders.get(info_hash) == null) {
+				this.seeders.put(info_hash, new Vector<Peer>(SEEDER_START, SEEDER_GROW));
+			}
+			
+			if(this.leechers.get(info_hash) != null && this.leechers.get(info_hash).contains(peer)) {
+				this.leechers.get(info_hash).remove(peer);
+			}
+			
+			if(this.seeders.get(info_hash).contains(peer) == false) {
+				this.seeders.get(info_hash).add(peer);
+			}
+		} else {
+			if(this.leechers.get(info_hash) == null) {
+				this.leechers.put(info_hash, new Vector<Peer>(LEECHER_START, LEECHER_GROW));
+			}
+			
+			if(this.seeders.get(info_hash).contains(peer)) {
+				this.seeders.get(info_hash).remove(peer);
+			}
+			
+			if(this.leechers.get(info_hash).contains(peer) == false) {
+				this.leechers.get(info_hash).add(peer);
+			}
+		}
+		
+	}
+	
+	public Peer[] getSeeders(final String info_hash, final int numPeers) {
+		final Vector<Peer> seeds = this.seeders.get(info_hash);
+		final int size = seeds.size();
 		
 		//make sure there are more peers wanted than there are seeders
 		//we need to go ahead and return this or we'll have a forever-true whileloop later.
 		if(numPeers > size) {
-			return (Object[][]) this.seeders.get(info_hash).toArray();
+			return this.seeders.get(info_hash).toArray(new Peer[size]);
 		}
 		
 		//this is a sad little optimization that we have to prevent our while loop from taking
@@ -42,12 +104,12 @@ public class PeerList {
 		//or when the number of peers is more than 2/3 the complete size.
 		if(numPeers + 10 > size || (numPeers*3)/2 > size) {
 			final int rand = (new Random()).nextInt(size - numPeers);
-			return (Object[][]) this.seeders.get(info_hash).subList(rand, numPeers-rand-1).toArray();
+			return this.seeders.get(info_hash).subList(rand, numPeers-rand).toArray(new Peer[numPeers]);
 		}
 		
 		//This is a temporary hash-set that we use to store the peers we're going to be 
 		//returning.  We use a hash set to ensure that peers don't get added more than once.
-		HashSet<Object[]> peers = new HashSet<Object[]>(numPeers);
+		HashSet<Peer> peers = new HashSet<Peer>(numPeers);
 		
 		//finally, we loop, generating random numbers, until we have enough peers
 		while(peers.size() < numPeers) {
@@ -55,6 +117,6 @@ public class PeerList {
 			peers.add(this.seeders.get(info_hash).get(rand));
 		}
 		
-		return (Object[][]) peers.toArray();
+		return peers.toArray(new Peer[numPeers]);
 	}
 }

52d3a9b886556557a8b1ad2516fc36a9873c7a14
oorza2k5@gmail.com <oorza2k5@gmail.com@d2126afe-2272-11df-8901-e16673284e38>
2010-03-10 09:26:33 +0000

    Deleted src/com as a refactoring effort.

--- src/com/shadowolf/tracker/AnnounceException.java
+++
@@ -1,8 +0,0 @@
-package com.shadowolf.tracker;
-
-@SuppressWarnings("serial")
-public class AnnounceException extends Exception {
-	public AnnounceException(String message) {
-		super(message);
-	}
-}

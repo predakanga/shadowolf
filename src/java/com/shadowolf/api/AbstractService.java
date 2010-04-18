d0463adfbacd41d91a6dfcddda809dddc57f0aaf
oorza2k5 <oorza2k5@d2126afe-2272-11df-8901-e16673284e38>
2010-04-18 21:56:48 +0000

    AbstractService checkin woot

---
+++ src/java/com/shadowolf/api/AbstractService.java
@@ -0,0 +1,121 @@
+package com.shadowolf.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+abstract public class AbstractService extends HttpServlet {
+	private static final long serialVersionUID = -5829686393247129661L;
+
+	@Override
+	protected void doDelete(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+		super.doDelete(request, response);
+
+		final BufferedReader bodyReader = request.getReader();
+		final ServletOutputStream outputStream = response.getOutputStream();
+		while(bodyReader.ready()) {
+			final String next = bodyReader.readLine();
+
+			if(this.deleteVerify(next)) {
+				outputStream.println(next + " " + this.deleteMethod(next));
+			} else {
+				outputStream.println(next + " " + ResponseCodes.FAILED_VERIFICATION);
+			}
+		}
+	
+		outputStream.flush();
+		outputStream.close();
+	}
+
+	@Override
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+		super.doGet(request, response);
+
+		final BufferedReader bodyReader = request.getReader();
+		final ServletOutputStream outputStream = response.getOutputStream();
+		while(bodyReader.ready()) {
+			final String next = bodyReader.readLine();
+
+			if(this.getVerify(next)) {
+				outputStream.println(next + " " + this.getMethod(next));
+			} else {
+				outputStream.println(next + " " + ResponseCodes.FAILED_VERIFICATION);
+			}
+		}
+		
+		outputStream.flush();
+		outputStream.close();
+	}
+
+	@Override
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+		super.doPost(request, response);
+
+		final BufferedReader bodyReader = request.getReader();
+		final ServletOutputStream outputStream = response.getOutputStream();
+		while(bodyReader.ready()) {
+			final String next = bodyReader.readLine();
+
+			if(this.postVerify(next)) {
+				outputStream.println(next + " " + this.postMethod(next));
+			} else {
+				outputStream.println(next + " " + ResponseCodes.FAILED_VERIFICATION);
+			}
+		}
+		
+		outputStream.flush();
+		outputStream.close();
+	}
+
+	@Override
+	protected void doPut(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+		super.doPost(request, response);
+
+		final BufferedReader bodyReader = request.getReader();
+		final ServletOutputStream outputStream = response.getOutputStream();
+		while(bodyReader.ready()) {
+			final String next = bodyReader.readLine();
+
+			if(this.putVerify(next)) {
+				outputStream.println(next + " " + this.putMethod(next));
+			} else {
+				outputStream.println(next + " " + ResponseCodes.FAILED_VERIFICATION);
+			}
+		}
+		
+		outputStream.flush();
+		outputStream.close();
+	}
+
+
+	protected int deleteMethod(final String inputLine) {
+		return ResponseCodes.UNSUPPORTED_METHOD;
+	}
+	protected int putMethod(final String inputLine) {
+		return ResponseCodes.UNSUPPORTED_METHOD;
+	}
+	protected int postMethod(final String inputLine) {
+		return ResponseCodes.UNSUPPORTED_METHOD;
+	}
+	protected int getMethod(final String inputLine) {
+		return ResponseCodes.UNSUPPORTED_METHOD;
+	}
+
+	protected boolean deleteVerify(final String inputLine) {
+		return true;
+	}
+	protected boolean putVerify(final String inputLine) {
+		return true;
+	}
+	protected boolean postVerify(final String inputLine) {
+		return true;
+	}
+	protected boolean getVerify(final String inputLine) {
+		return true;
+	}
+}
---
+++ src/java/com/shadowolf/api/ResponseCodes.java
@@ -0,0 +1,9 @@
+package com.shadowolf.api;
+
+final public class ResponseCodes {
+	final public static int SUCCESS = 0;
+	final public static int UNDEFINED_ERROR = 1;
+	final public static int UNSUPPORTED_METHOD = 2;
+	final public static int FAILED_VERIFICATION = 3;
+	
+}

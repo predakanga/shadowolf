c95b991f54e58f5187d7e5716b1e9556503ee556
oorza2k5 <oorza2k5@d2126afe-2272-11df-8901-e16673284e38>
2010-04-18 23:36:30 +0000

    Added NOTHING_TO_DELETE response code to help in testing... in practice this will probably be ignored 99% of the time.

--- src/java/com/shadowolf/api/ResponseCodes.java
+++ src/java/com/shadowolf/api/ResponseCodes.java
@@ -5,5 +5,5 @@ final public class ResponseCodes {
 	final public static int UNDEFINED_ERROR = 1;
 	final public static int UNSUPPORTED_METHOD = 2;
 	final public static int FAILED_VERIFICATION = 3;
-	
+	final public static int NOTHING_TO_DELETE = 4;
 }

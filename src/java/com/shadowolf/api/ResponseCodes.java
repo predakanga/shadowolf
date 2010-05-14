ca5541e581bf0f9fb0603b1564f14056875a2066
ZoFreX <ZoFreX@d2126afe-2272-11df-8901-e16673284e38>
2010-05-14 19:01:42 +0000

    Deleted old API

--- src/java/com/shadowolf/api/ResponseCodes.java
+++
@@ -1,9 +0,0 @@
-package com.shadowolf.api;
-
-final public class ResponseCodes {
-	final public static int SUCCESS = 0;
-	final public static int UNDEFINED_ERROR = 1;
-	final public static int UNSUPPORTED_METHOD = 2;
-	final public static int FAILED_VERIFICATION = 3;
-	final public static int NOTHING_TO_DELETE = 4;
-}

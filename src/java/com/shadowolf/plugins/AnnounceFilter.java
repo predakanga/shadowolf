644869b2b7b22c2550aa715d9ec48bfac4857c14
oorza2k5@gmail.com <oorza2k5@gmail.com@d2126afe-2272-11df-8901-e16673284e38>
2011-07-31 19:24:43 +0000

    Blow all this old shit away

--- src/java/com/shadowolf/plugins/AnnounceFilter.java
+++
@@ -1,9 +0,0 @@
-package com.shadowolf.plugins;
-
-import com.shadowolf.announce.Announce;
-import com.shadowolf.tracker.AnnounceException;
-
-public interface AnnounceFilter {
-	//PMD is complaining about the public keyword here... really?!
-	public void filterAnnounce(Announce announce) throws AnnounceException;
-}
--- src/java/com/shadowolf/plugins/Plugin.java
+++
@@ -1,6 +0,0 @@
-package com.shadowolf.plugins;
-
-//more PMD telling me how to name my things
-public abstract class Plugin { //NOPMD
-	
-}

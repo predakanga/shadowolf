225427fee896aa2d1d023372b71f78fb76ff9836
oorza2k5@gmail.com <oorza2k5@gmail.com@d2126afe-2272-11df-8901-e16673284e38>
2011-08-01 02:27:50 +0000

    Empty servlets to be filled in later

---
+++ src/java/com/shadowolf/server/request/AnnounceRequest.java
@@ -0,0 +1,8 @@
+package com.shadowolf.server.request;
+
+import javax.servlet.http.HttpServlet;
+
+public class AnnounceRequest extends HttpServlet {
+	private static final long serialVersionUID = 311933800012488491L;
+
+}

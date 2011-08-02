6ff8fa83dbb30122597785fcbf1965f47eecb97a
jona102190@gmail.com <jona102190@gmail.com@d2126afe-2272-11df-8901-e16673284e38>
2011-08-02 00:52:14 +0000

    Rolled back previous commit.

--- src/java/com/shadowolf/plugin/points/ScheduledTask.java
+++ src/java/com/shadowolf/plugin/points/ScheduledTask.java
@@ -2,35 +2,8 @@ package com.shadowolf.plugin.points;
 
 import java.util.concurrent.TimeUnit;
 
-import javax.annotation.concurrent.GuardedBy;
-
-public abstract class ScheduledTask {
-	protected TimeUnit timeUnit;
-	protected int interval;
-	
-	@GuardedBy("this")
-	private boolean running;
-	
-	public TimeUnit getTimeUnit() {
-		return timeUnit;
-	}
-	
-	public int getInterval() {
-		return interval;
-	}
-	
-	public void run() {
-		synchronized(this) {
-			if(running) {
-				//log this?
-				return;
-			} else {
-				running = true;
-			}
-		}
-		
-		this.execute();
-	}
-	
-	public abstract void execute();
+public interface ScheduledTask {
+	public TimeUnit getTimeUnit();
+	public int getInterval();
+	public void run();
 }

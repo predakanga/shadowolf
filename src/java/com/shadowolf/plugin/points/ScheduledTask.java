be795e7021a087aa578977c242c2c2baec37a757
oorza2k5@gmail.com <oorza2k5@gmail.com@d2126afe-2272-11df-8901-e16673284e38>
2011-08-02 05:14:21 +0000

    Added delay

--- src/java/com/shadowolf/plugin/points/ScheduledTask.java
+++ src/java/com/shadowolf/plugin/points/ScheduledTask.java
@@ -5,5 +5,6 @@ import java.util.concurrent.TimeUnit;
 public interface ScheduledTask extends Runnable {
 	public TimeUnit getTimeUnit();
 	public int getInterval();
+	public int getDelay();
 	public void run();
 }

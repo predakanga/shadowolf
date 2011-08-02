54f8cb8ed11cd5d697d80956322dda44371e92d4
oorza2k5@gmail.com <oorza2k5@gmail.com@d2126afe-2272-11df-8901-e16673284e38>
2011-08-02 06:10:49 +0000

    Javadoc!

--- src/java/com/shadowolf/plugin/Plugin.java
+++ src/java/com/shadowolf/plugin/Plugin.java
@@ -4,23 +4,41 @@ import java.net.URLClassLoader;
 
 import com.google.common.collect.Multimap;
 
+/**
+ * Class that represents a plugin.
+ *
+ */
 public class Plugin {
 	private String name;
 	private URLClassLoader classloader;
 	private Multimap<Class<?>, Object> extensions;
 
+	/**
+	 * The plugin's name.
+	 */
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * The classloader used to load this plugin's classes.
+	 */
 	public URLClassLoader getClassloader() {
 		return classloader;
 	}
 	public void setClassloader(URLClassLoader classloader) {
 		this.classloader = classloader;
 	}
+	
+	/**
+	 * A Multimap with keys of the extension point interfaces
+	 * and values of objects that implement those interfaces.
+	 * An object may appear here more than once under several
+	 * different keys.
+	 */
 	public Multimap<Class<?>, Object> getExtensions() {
 		return extensions;
 	}

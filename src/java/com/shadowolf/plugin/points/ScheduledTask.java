d8116046a7bbc25ec454ff206f8c96e9a5f448e7
jona102190@gmail.com <jona102190@gmail.com@d2126afe-2272-11df-8901-e16673284e38>
2011-08-02 00:35:42 +0000

    Changed SchedueledTask to an abstract class.

--- src/java/com/shadowolf/plugin/points/ScheduledTask.java
+++ src/java/com/shadowolf/plugin/points/ScheduledTask.java
@@ -2,8 +2,35 @@ package com.shadowolf.plugin.points;
 
 import java.util.concurrent.TimeUnit;
 
-public interface ScheduledTask {
-	public TimeUnit getTimeUnit();
-	public int getInterval();
-	public void run();
+import javax.annotation.concurrent.GuardedBy;
+
+public abstract class ScheduledTask {
+	protected TimeUnit timeUnit;
+	protected int interval;
+	
+	@GuardedBy("this")
+	private boolean running;
+	
+	public TimeUnit getTimeUnit() {
+		return timeUnit;
+	}
+	
+	public int getInterval() {
+		return interval;
+	}
+	
+	public void run() {
+		synchronized(this) {
+			if(running) {
+				//log this?
+				return;
+			} else {
+				running = true;
+			}
+		}
+		
+		this.execute();
+	}
+	
+	public abstract void execute();
 }

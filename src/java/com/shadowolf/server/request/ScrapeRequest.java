4a2eb175d59f7165ac02167c266037bbdcb58a41
oorza2k5 <oorza2k5@d2126afe-2272-11df-8901-e16673284e38>
2011-08-03 06:29:29 +0000

    implemented ShadowolfComponent

--- src/java/com/shadowolf/server/request/ScrapeRequest.java
+++ src/java/com/shadowolf/server/request/ScrapeRequest.java
@@ -2,7 +2,22 @@ package com.shadowolf.server.request;
 
 import javax.servlet.http.HttpServlet;
 
-public class ScrapeRequest extends HttpServlet {
+import com.shadowolf.ShadowolfComponent;
+import com.shadowolf.ShadowolfContext;
+
+public class ScrapeRequest extends HttpServlet implements ShadowolfComponent {
 	private static final long serialVersionUID = 311933800012488491L;
+
+private ShadowolfContext context;
+	
+	@Override
+	public void setContext(ShadowolfContext context) {
+		this.context = context;
+	}
+
+	@Override
+	public ShadowolfContext getContext() {
+		return context;
+	}
 
 }

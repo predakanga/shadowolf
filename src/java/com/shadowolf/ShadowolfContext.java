d7c87b6cff5e5a9312ccc1c5ebce497e24b29e4a
oorza2k5 <oorza2k5@d2126afe-2272-11df-8901-e16673284e38>
2011-08-08 02:26:53 +0000

    Decomposed

--- src/java/com/shadowolf/ShadowolfContext.java
+++ src/java/com/shadowolf/ShadowolfContext.java
@@ -2,7 +2,6 @@ package com.shadowolf;
 
 import java.io.File;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentMap;
@@ -25,7 +24,8 @@ import com.google.common.collect.MapMaker;
 import com.shadowolf.plugin.Plugin;
 import com.shadowolf.plugin.PluginEngine;
 import com.shadowolf.plugin.PluginLoader;
-import com.shadowolf.protocol.Peer;
+import com.shadowolf.protocol.Infohash;
+import com.shadowolf.protocol.Peerlist;
 import com.shadowolf.server.AccessList;
 import com.shadowolf.server.JettyServer;
 import com.shadowolf.util.Exceptions;
@@ -62,53 +62,14 @@ import com.shadowolf.util.Exceptions;
 
 @Immutable
 public class ShadowolfContext {
-	private PluginLoader pluginLoader;
-	private PluginEngine pluginEngine;
-	private ExecutorService httpWorkerPool;
-	private ExecutorService asyncPluginWorkerPool;
-	private JettyServer jettyServer;
+	private ServerContext serverContext = new ServerContext();
+	private PluginContext pluginContext = new PluginContext();
+	private BittorrentContext bittorrentContext = new BittorrentContext();
+	
 	private Configuration configuration;
-	private AccessList serverAccessList;
-	private MapMaker peerlistMapMaker;
 	
 	protected ShadowolfContext() {
 		
-				
-	}
-	
-	public PluginLoader getPluginLoader() {
-		return pluginLoader;
-	}
-	protected void setPluginLoader(PluginLoader pluginLoader) {
-		this.pluginLoader = pluginLoader;
-	}
-
-	public PluginEngine getPluginEngine() {
-		return pluginEngine;
-	}
-	protected void setPluginEngine(PluginEngine pluginEngine) {
-		this.pluginEngine = pluginEngine;
-	}
-
-	public ExecutorService getHttpWorkerPool() {
-		return httpWorkerPool;
-	}
-	protected void setHttpWorkerPool(ExecutorService httpWorkerPool) {
-		this.httpWorkerPool = httpWorkerPool;
-	}
-
-	public ExecutorService getAsyncPluginWorkerPool() {
-		return asyncPluginWorkerPool;
-	}
-	protected void setAsyncPluginWorkerPool(ExecutorService asyncPluginWorkerPool) {
-		this.asyncPluginWorkerPool = asyncPluginWorkerPool;
-	}
-
-	public JettyServer getJettyServer() {
-		return jettyServer;
-	}
-	protected void setJettyServer(JettyServer jettyServer) {
-		this.jettyServer = jettyServer;
 	}
 
 	public Configuration getConfiguration() {
@@ -118,16 +79,28 @@ public class ShadowolfContext {
 		this.configuration = configuration;
 	}
 
-	public ConcurrentMap<InetSocketAddress, Peer> makepeerListMap() {
-		return peerlistMapMaker.makeMap();
+	public ServerContext getServerContext() {
+		return serverContext;
 	}
-	
+
+	public PluginContext getPluginContext() {
+		return pluginContext;
+	}
+
+	public BittorrentContext getBittorrentContext() {
+		return bittorrentContext;
+	}
+
 	public void start() throws Exception {
-		pluginEngine.startScheduler();
-		jettyServer.startServer(serverAccessList);
-		jettyServer.join();
+		pluginContext.getPluginEngine().startScheduler();
+		serverContext.getJettyServer().startServer(serverContext.getServerAccessList());
+		serverContext.getJettyServer().join();
 	}
 	
+	/**
+	 * This static factory method is huge, and it is ugly.  It creates
+	 * an entire context for an application!
+	 */
 	public static ShadowolfContext createNewContext(String configFile) {
 		ShadowolfContext context = new ShadowolfContext();
 		
@@ -138,10 +111,12 @@ public class ShadowolfContext {
 			throw new RuntimeException(e);
 		}
 		
+		//initialize ServerContext
 		try {
+			ServerContext serverContext = new ServerContext();
 			InetAddress address = InetAddress.getByName(context.configuration.getString("server.listen.address", "127.0.0.1"));
 			int port = context.configuration.getInt("server.listen.port", 80);
-			context.jettyServer = new JettyServer(address, port);
+			serverContext.setJettyServer(new JettyServer(address, port));
 			
 			int corePoolSize = context.configuration.getInt("server.workers.min", 4);
 			int maximumPoolSize = context.configuration.getInt("server.workers.max", 16);
@@ -149,62 +124,153 @@ public class ShadowolfContext {
 			int queueLength = context.configuration.getInt("server.workers.queueSize", 25_000);
 			BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(queueLength);
 			TimeUnit unit = TimeUnit.SECONDS;
+
+			ExecutorService es = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+			serverContext.setHttpWorkerPool(es);
+			serverContext.getJettyServer().setExecutor(es);
 			
-			context.httpWorkerPool = 
-					new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
-			context.jettyServer.setExecutor(context.httpWorkerPool);
-			
-		} catch (UnknownHostException | ServletException e) {
-			Exceptions.log(e);
-			throw new RuntimeException(e);
-		}
-		
-		try {
 			String conf = "conf/serverAccess.xml";
 			JAXBContext jaxb = JAXBContext.newInstance(AccessList.class);
 			Unmarshaller um = jaxb.createUnmarshaller();
-			context.serverAccessList = (AccessList) um.unmarshal(new File(conf));
-		} catch (JAXBException e) {
+			serverContext.setServerAccessList((AccessList) um.unmarshal(new File(conf)));
+			
+			context.serverContext = serverContext;
+		} catch (UnknownHostException | ServletException | JAXBException e) {
 			Exceptions.log(e);
 			throw new RuntimeException(e);
 		}
-		;
 		
-		int corePoolSize = context.configuration.getInt("plugins.async.workers.min", 4);
-		int maximumPoolSize = context.configuration.getInt("plugins.async.workers.max", 16);
-		int keepAliveTime = context.configuration.getInt("plugins.async.workers.idle", 300);
-		int queueLength = context.configuration.getInt("plugins.async.workers.queueSize", 25_000);
-		BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(queueLength);
-		TimeUnit unit = TimeUnit.SECONDS;
-		
-		context.asyncPluginWorkerPool = 
-				new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);;
-		
-		context.pluginEngine = new PluginEngine(context.asyncPluginWorkerPool);;
-		
-		context.pluginLoader = new PluginLoader();
-		context.pluginLoader.setContext(context);
-		context.pluginLoader.loadAllPluginsInDir();
-		
-		for(Plugin p : context.pluginLoader.getPlugins().values()) {
-			context.pluginEngine.addPlugin(p);
+		//initialize PluginContext 
+		{
+			PluginContext pluginContext = new PluginContext();
+			
+			int corePoolSize = context.configuration.getInt("plugins.async.workers.min", 4);
+			int maximumPoolSize = context.configuration.getInt("plugins.async.workers.max", 16);
+			int keepAliveTime = context.configuration.getInt("plugins.async.workers.idle", 300);
+			int queueLength = context.configuration.getInt("plugins.async.workers.queueSize", 25_000);
+			BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(queueLength);
+			TimeUnit unit = TimeUnit.SECONDS;
+			
+			ExecutorService es = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+
+			PluginLoader loader = new PluginLoader();
+			loader.setContext(context);
+			loader.loadAllPluginsInDir();
+			
+			PluginEngine engine = new PluginEngine(es);
+			for(Plugin p : loader.getPlugins().values()) {
+				engine.addPlugin(p);
+			}
+			
+			pluginContext.setAsyncPluginWorkerPool(es);
+			pluginContext.setPluginEngine(engine);
+			pluginContext.setPluginLoader(loader);
+			context.pluginContext = pluginContext;
 		}
 		
-		int peerExpiry = context.getConfiguration().getInt("protocol.Peerlist.peerExpiry", 2400);
-		int httpWorkers = context.getConfiguration().getInt("server.workers.max", 16);
-		// 1/8th of all workers accessing the same peerlist seems unlikely
-		// and a concurrency level of six seems sane... these values might need to be tuned later
-		int concurrencyLevel = (httpWorkers/8) > 6 ? 6 : httpWorkers/8; 
+		//initialize BittorrentContext
+		{
+			BittorrentContext bittorrentContext = new BittorrentContext();
 		
-		//make mapmaker
-		context.peerlistMapMaker = new MapMaker().
+			int peerExpiry = context.getConfiguration().getInt("protocol.Peerlist.peerExpiry", 2400);
+			int httpWorkers = context.getConfiguration().getInt("server.workers.max", 16);
+			// 1/8th of all workers accessing the same peerlist seems unlikely
+			// and a concurrency level of six seems sane... these values might need to be tuned later
+			int concurrencyLevel = (httpWorkers/8) > 6 ? 6 : httpWorkers/8; 
+			
+			//the largest majority of torrents that will ever be tracked will have
+			//less than 2 peers, so reducing the default size means that we'll have
+			//slightly less memory overhead - in exchange for a bunch of resize ops
+			bittorrentContext.setPeerlistMapMaker(
+				new MapMaker().
 				expireAfterWrite(peerExpiry, TimeUnit.SECONDS).
 				concurrencyLevel(concurrencyLevel).
-				initialCapacity(4); 
-				//the largest majority of torrents that will ever be tracked will have
-				//less than 4 peers, so reducing the default size means that we'll have
-				//slightly less memory overhead
+				initialCapacity(2)
+			);
+			
+			ConcurrentMap<Infohash, Peerlist> plists = 
+					new MapMaker().
+					expireAfterAccess(peerExpiry, TimeUnit.SECONDS).
+					concurrencyLevel(concurrencyLevel).
+					makeMap();
+			bittorrentContext.setPeerlists(plists);
+			
+			context.bittorrentContext = bittorrentContext;
+		}
 		
 		return context;
+	}
+	
+	public static class ServerContext {
+		private JettyServer jettyServer;
+		private ExecutorService httpWorkerPool;
+		private AccessList serverAccessList;
+		
+		public ExecutorService getHttpWorkerPool() {
+			return httpWorkerPool;
+		}
+		protected void setHttpWorkerPool(ExecutorService httpWorkerPool) {
+			this.httpWorkerPool = httpWorkerPool;
+		}
+		
+		public JettyServer getJettyServer() {
+			return jettyServer;
+		}
+		protected void setJettyServer(JettyServer jettyServer) {
+			this.jettyServer = jettyServer;
+		}
+		
+		public AccessList getServerAccessList() {
+			return serverAccessList;
+		}
+		public void setServerAccessList(AccessList serverAccessList) {
+			this.serverAccessList = serverAccessList;
+		}
+	}
+	
+	public static class PluginContext {
+		private PluginLoader pluginLoader;
+		private PluginEngine pluginEngine;
+		private ExecutorService asyncPluginWorkerPool;
+		
+		public PluginLoader getPluginLoader() {
+			return pluginLoader;
+		}
+		protected void setPluginLoader(PluginLoader pluginLoader) {
+			this.pluginLoader = pluginLoader;
+		}
+
+		public PluginEngine getPluginEngine() {
+			return pluginEngine;
+		}
+		protected void setPluginEngine(PluginEngine pluginEngine) {
+			this.pluginEngine = pluginEngine;
+		}
+		
+		public ExecutorService getAsyncPluginWorkerPool() {
+			return asyncPluginWorkerPool;
+		}
+		protected void setAsyncPluginWorkerPool(ExecutorService asyncPluginWorkerPool) {
+			this.asyncPluginWorkerPool = asyncPluginWorkerPool;
+		}
+	}
+	
+	public static class BittorrentContext {
+		private MapMaker peerlistMapMaker;
+		private ConcurrentMap<Infohash, Peerlist> peerlists;
+		
+		public MapMaker getPeerlistMapMaker() {
+			return peerlistMapMaker;
+		}
+		public void setPeerlistMapMaker(MapMaker peerlistMapMaker) {
+			this.peerlistMapMaker = peerlistMapMaker;
+		}
+		
+		public ConcurrentMap<Infohash, Peerlist> getPeerlists() {
+			return peerlists;
+		}
+		public void setPeerlists(ConcurrentMap<Infohash, Peerlist> peerlists) {
+			this.peerlists = peerlists;
+		}
 	}
 }
